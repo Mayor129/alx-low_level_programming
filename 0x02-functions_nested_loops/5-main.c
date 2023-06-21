2b894cfe9ed5cc4626522a542a011b71e0e8564e
Mayor129 <musefiu.muyiwa@yahoo.com>
2023-06-21 16:11:36 -0700

    task 5

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - check the code.
  *
@@ -10,23 +11,24 @@ int main(void)
 
 	r = print_sign(98);
 	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+r = print_sign(0);
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+r = print_sign(0xff);
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+r = print_sign(-1);
+_putchar(',');
+_putchar(' ');
+_putchar(r + '0');
+_putchar('\n');
+
+return (0);
 }

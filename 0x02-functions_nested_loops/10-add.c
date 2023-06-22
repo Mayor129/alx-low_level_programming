ae56fcd2da3212c180470d013ec18482603f0aec
Mayor129 <musefiu.muyiwa@yahoo.com>
2023-06-21 18:45:30 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Adds two variables
+ * @n1: The first number to be compluted
+ * @n2: The Second number to be computed
+ * Return: Integer value
+ */
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}

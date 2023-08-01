13e8326afed0500f6f0a5194df10eb3f8405f56b
Mayor129 <musefiu.muyiwa@yahoo.com>
2023-08-01 14:50:06 -0700

    task 3 mod

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,18 +3,18 @@
 /**
  * listint_len - the function that returns the number
  * of elements in a linked lists
- * @t: the variable linked list of type listint_t to traverse
+ * @f: the variable linked list of type listint_t to traverse
  *
  * Return: Function to return  number of nodes
  */
-size_t listint_len(const listint_t *t)
+size_t listint_len(const listint_t *f)
 {
 size_t num = 0;
 
-while (t)
+while (f)
 {
 num++;
-t = t->next;
+f = f->next;
 }
 return (num);
 }
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,12 +3,12 @@
 /**
  * add_nodeint_end - the Fubctions to  adds a node at the end of a linked list
  * @head: the pointer to the first element in the list
- * @f: the data variable to insert in the new element
+ * @n: the data variable to insert in the new element
  *
  * Return: the function to point the pointer to the new node,
  * or NULL if it fails
  */
-listint_t *add_nodeint_end(listint_t **head, const int f)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *new;
 listint_t *temp = *head;
@@ -17,7 +17,7 @@ new = malloc(sizeof(listint_t));
 if (!new)
 return (NULL);
 
-new->f = f;
+new->n = n;
 new->next = NULL;
 
 if (*head == NULL)
